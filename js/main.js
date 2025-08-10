f593c0941daa010219fe44865b1733208fe40271
muxlisa0206 <sayfullayevamuxlisa27@gmail.com>
2025-08-10 19:37:21 +0500

    turkish kebab

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const Menutoggle = document.getElementById("toggle")
+const Menu = document.getElementById("menu")
+const Close = document.getElementById("close")
+
+Menutoggle.addEventListener("click", () => {
+    Menu.classList.add("menu")
+})
+Close.addEventListener("click", () => {
+    Menu.classList.remove("menu")
+    Menu.classList.add("menu_2")
+})
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+}); 
+  
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    dots:false,
+    autoplay: false,
+    autoWidth:true,
+    autoplayTimeout:3000,
+  });
+
+
